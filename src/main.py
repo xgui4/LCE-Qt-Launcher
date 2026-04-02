866e55ea7780c5e3a5fb95f29cfa5b2873103492
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-04-02 01:26:02 -0400

    Refactor Instances Editor UI and Update Versioning
    - Changed the instance editor main window class name from "Instance" to "InstancesEditor" in the UI file.
    - Updated the instance editor window title to "LCE Qt Launcher - Instances Editor" and added an application icon.
    - Updated product and file version to "0.26.4.2" in main.py.
    - Updated package version for "lce-qt-launcher" to "26.4.2a1" in uv.lock.
    - Removed unused dependencies "term-image" from the project.

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@
 # nuitka-project: --include-data-dir=assets=assets
 # nuitka-project: --include-qt-plugins=sensible
 # nuitka-project: --windows-console-mode=force
-# nuitka-project: --product-version="0.26.3.2"
-# nuitka-project: --file-version="0.26.3.2"
+# nuitka-project: --product-version="0.26.4.2"
+# nuitka-project: --file-version="0.26.4.2"
 # nuitka-project: --file-description="Custom Free/Libre Minecraft LCE Launcher (Nightly)"
 # nuitka-project: --copyright="Copyleft Xgui4 2026 (GPLv3)"
 
