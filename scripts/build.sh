1c770f612284b632467e8015e49cfc9c1ad6d4b3
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-04-20 00:09:09 -0400

    fix a part of my build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [[ $(uname -s) != "FreeBSD" ]]; then 
-    /usr/lib/qt6/uic -g python src/form.ui -o src/lce_qt_launcher/ui_form.py &&
     /usr/lib/qt6/rcc -g python res.qrc -o src/lce_qt_launcher/res_rc.py && 
+    /usr/lib/qt6/uic -g python src/form.ui -o src/lce_qt_launcher/ui_form.py &&
     /usr/lib/qt6/uic -g python src/system_info.ui -o src/lce_qt_launcher/ui_system_info.py && 
     /usr/lib/qt6/uic -g python src/instance.ui -o src/lce_qt_launcher/ui_instance.py && 
     /usr/lib/qt6/uic -g python src/settingDialog.ui -o src/lce_qt_launcher/ui_settingDialog.py
@@ -15,5 +15,3 @@ else
     /usr/local/pyside6/uic -g python "src/settingDialog.ui" -o "src/lce_qt_launcher/ui_settingDialog.py" 
     /usr/local/pyside6/uic -g python "src/about.ui" -o "src/lce_qt_launcher/ui_about.py" 
 fi
-
-
--- scripts/packages.ps1
+++ scripts/packages.ps1
@@ -2,4 +2,4 @@ $ErrorActionPreference = 'Stop'
 $PSNativeCommandUseErrorActionPreference = $true
 
 hatch run python -m nuitka --standalone --assume-yes-for-download --output-dir=dist src\lce_qt_launcher\main.py
-Compress-Archive -Path dist/lce-qt-launcher.dist\* -DestinationPath LCE-Qt-Launcher-Windows.zip
+Compress-Archive -Path dist/main.dist\* -DestinationPath LCE-Qt-Launcher-Windows.zip
