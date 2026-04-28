eede2900fd299bbe90593d298c7b90384f1e7170
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-04-27 23:00:35 -0400

    start nixos experiment

--- default.nix
+++ default.nix
@@ -1,5 +1,3 @@
-# LCE-Qt-Launcher Nix Packages
-
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.python3Packages.buildPythonApplication rec {
@@ -9,13 +7,19 @@ pkgs.python3Packages.buildPythonApplication rec {
 
   src = ./.;
 
-  # Outils nécessaires à la compilation
   nativeBuildInputs = [
     pkgs.python3Packages.hatchling
-    pkgs.qt6.wrapQtAppsHook  # Indispensable ici pour le wrap automatique
+    pkgs.python3Packages.pyside6
+    pkgs.python3Packages.shiboken6 
+    pkgs.qt6.wrapQtAppsHook
+    pkgs.qt6.qtbase
+    pkgs.qt6.qtbase.dev
+    pkgs.qt6.qttools
+    pkgs.bash
+    pkgs.coreutils
+    pkgs.gnused
   ];
 
-  # Dépendances Python (Note: pas de virgules dans les listes Nix)
   propagatedBuildInputs = with pkgs.python3Packages; [
     pyside6
     requests
@@ -23,12 +27,38 @@ pkgs.python3Packages.buildPythonApplication rec {
     rich
   ];
 
-  # On s'assure que le wrapper Qt est appliqué aux exécutables Python
-  dontWrapQtApps = true; # On désactive le wrap automatique global...
+  postPatch = ''
+    patchShebangs scripts/
+    chmod +x scripts/*.sh
+    substituteInPlace scripts/build.sh \
+      --replace-fail "/usr/lib/qt6/rcc" "rcc" \
+      --replace-fail "/usr/lib/qt6/uic" "uic"
+  '';
 
+  # Manual installation of non-Python assets (Desktop files, icons, etc.)
+  postInstall = ''
+    # Desktop Entry & Mime
+    install -Dm644 "packages/io.github.xgui4.lce_qt_launcher.desktop" "$out/share/applications/io.github.xgui4.lce_qt_launcher.desktop"
+    install -Dm644 "packages/lce_inst-mime.xml" "$out/share/mime/packages/io.github.xgui4.lce_qt_launcher.xml"
+    install -Dm644 "packages/io.github.xgui4.lce_qt_launcher.metainfo.xml" "$out/share/metainfo/io.github.xgui4.lce_qt_launcher.metainfo.xml"
+
+    # Icons
+    install -Dm644 "assets/io.github.xgui4.lce_qt_launcher.png" "$out/share/icons/hicolor/128x128/apps/io.github.xgui4.lce_qt_launcher.png"
+    install -Dm644 "assets/lce_inst.png" "$out/share/icons/hicolor/128x128/mimetypes/application-x-lce_inst.png"
+
+    # Data/Assets (Mirroring your /opt logic into Nix share)
+    mkdir -p "$out/share/${pname}"
+    cp -r data assets "$out/share/${pname}/"
+
+    # Docs
+    install -Dm644 "license.md" -t "$out/share/licenses/${pname}/"
+    install -Dm644 "readme.md" "code-of-conduct.md" -t "$out/share/doc/${pname}/"
+  '';
+
+  dontWrapQtApps = true;
   preFixup = ''
     makeWrapperArgs+=("''${qtWrapperArgs[@]}")
-  ''; # ...pour l'injecter proprement dans le wrapper Python (meilleure pratique)
+  '';
 
   meta = with pkgs.lib; {
     description = "A custom Minecraft LCE Launcher written in Python and Qt with GNU/Linux support in mind.";
--- scripts/prepare.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo "Changing directory to the images directory"
-cd assets/images || exit /b 1p
-echo "done"
-
-pwsh-bypass.bat
