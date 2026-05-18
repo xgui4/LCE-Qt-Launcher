dcb5dfe588d49269f1ceaa5540fd84cc4bfd64cd
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-05-18 13:25:47 -0400

    minor change, : change the neolegay icon

--- src/lce_qt_launcher/managers/instance_manager.py
+++ src/lce_qt_launcher/managers/instance_manager.py
@@ -155,7 +155,7 @@ _DEFAULT_INST_SOURCE = InstanceSource.GITHUB_RELEASE
 _DEFAULT_INST_TYPE = InstanceType.CLIENT_VANILLA
 _DEFAULT_INST_SOURCE_STRING = "InstanceSource.GITHUB_RELEASE"
 _DEFAULT_INST_TYPE_STRING = "InstanceType.CLIENT_VANILLA"
-_DEFAULT_IMAGE = ":/assets/lce-hub-ico.png"
+_DEFAULT_IMAGE = ":/assets/neoLegacy.png"
 _DEFAULT_NEWS_FEED = ("https://github.com/neoStudiosLCE/neoLegacy/commits/main/")
 _DEFAULT_VERSION = "v1.0.3b"
 
