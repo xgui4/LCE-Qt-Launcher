1d88f5bd5c931de55167b8c391e9e5ac7fb5d1e4
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-05-20 14:33:49 -0400

    rework the linux build script and try to fix the build for the freebsd port

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,43 +5,48 @@ set -e
 QT_RESSOURCE="res.qrc"
 UI_FILES=("form" "system_info" "instance" "settingDialog" "about" "contentInstaller")
 
-if [[ $(uname -s) != "FreeBSD" ]]; then 
-    if command -v  pyside6-rcc  &> /dev/null; then
-        echo "Starting Compilaton of Qt Ressource"
-        pyside6-rcc $QT_RESSOURCE -o src/lce_qt_launcher/res_rc.py
-        echo "done"
-                
-        for ui in "${UI_FILES[@]}"; do
-            echo "Compiling ${ui}.ui"
-            OUT="src/lce_qt_launcher/ui_${ui}.py"
-            pyside6-uic -g python "src/${ui}.ui" -o "$OUT"
-            echo "done"
-            sed -i "s/^import res_rc/from . import res_rc/g" "$OUT"
-            echo "fixed the import done"
-        done
-    else
-        echo "Starting Compilaton of Qt Ressource"
-        /usr/lib/qt6/rcc -g python $QT_RESSOURCE -o src/lce_qt_launcher/res_rc.py
-        echo "done"
-                
-        for ui in "${UI_FILES[@]}"; do
-            echo "Compiling ${ui}.ui"
-            OUT="src/lce_qt_launcher/ui_${ui}.py"
-            /usr/lib/qt6/uic -g python "src/${ui}.ui" -o "$OUT"
-            echo "done"
-            sed -i "s/^import res_rc/from . import res_rc/g" "$OUT"
-            echo "fixed the import done"
-        done
+RCC="pyside6-rcc"
+UIC="pyside6-ui"
+
+fix_import() {
+    if [[ $(uname -o) == "GNU/Linux" ]]; then
+        sed -i 's/^import res_rc/from . import res_rc/g' "$1"
+    else 
+        sed -i.bak 's/^import res_rc/from . import res_rc/g' "$1"
+        rm "$1.bak"
     fi
-else
-    echo "Starting Compilaton of Qt Ressource"
-    /usr/local/PySide6/bin/rcc -g python $QT_RESSOURCE -o src/lce_qt_launcher/res_rc.py
+}
+
+find_qt_tool() {
+    if command -v pyside6-rcc &> /dev/null; then
+        RCC="pyside6-rcc"
+        UIC="pyside6-uic"
+    elif command -v /usr/local/bin/pyside6-rcc &> /dev/null; then
+        RCC="/usr/local/bin/pyside6-rcc"
+        UIC="/usr/local/bin/pyside6-uic"
+    elif command -v /usr/lib/qt6/rcc &> /dev/null; then
+        RCC="/usr/lib/qt6/rcc -g python"
+        UIC="/usr/lib/qt6/uic -g python"
+    elif command -v /usr/local/PySide6/bin/rcc &> /dev/null; then
+        RCC="/usr/local/PySide6/bin/rcc -g python"
+        UIC="/usr/local/PySide6/bin/uic -g python"
+    fi
+}
+
+echo "finding the correct path for the Qt compilers"
+find_qt_tool
+echo "done"
+
+echo "Starting Compilaton of Qt Ressource"
+$RCC -g python $QT_RESSOURCE -o src/lce_qt_launcher/res_rc.py
+echo "done"
+            
+for ui in "${UI_FILES[@]}"; do
+    echo "Compiling ${ui}.ui"
+    OUT="src/lce_qt_launcher/ui_${ui}.py"
+    $UIC "src/${ui}.ui" -o "$OUT"
     echo "done"
-                
-    for ui in "${UI_FILES[@]}"; do
-        echo "Compiling ${ui}.ui"
-        OUT="src/lce_qt_launcher/ui_${ui}.py"
-        /usr/local/PySide6/bin/uic -g python "src/${ui}.ui" -o "$OUT"
-        echo "done"
-    done
-fi
+    echo "fixing the import"
+    fix_import "$OUT"
+    echo "done"
+done
