521184b1def06f59795c1c618cb6c4a3b293d6f9
xgui4 <134389196+xgui4@users.noreply.github.com>
2026-05-30 01:31:09 -0400

    fix the windows build and remove useless print in __init__

--- hatch_build.py
+++ hatch_build.py
@@ -9,12 +9,12 @@ class CustomBuildHook(BuildHookInterface):  # pyright: ignore[reportMissingTypeA
         if os.name == "posix":
             _ = subprocess.run("./scripts/clean.sh", check=True)
         if os.name == "nt":
-            _ = subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", "\"scripts\\clean.ps1\""], check=True, shell=True)
+            _ = subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", "scripts\\clean.ps1"], check=True, shell=True)
         return super().clean(versions)
 
     def initialize(self, version, build_data) -> None:  # pyright: ignore[reportMissingParameterType]
         if os.name == "posix":
             _ = subprocess.run("./scripts/build.sh", check=True)
         if os.name == "nt":
-            _ = subprocess.run(["powershell", "ExecutionPolicy", "Bypass", "-File", "\"scripts\\build.ps1\""], check=True, shell=True)
+            _ = subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", "scripts\\build.ps1"], check=True, shell=True)
         return super().initialize(version, build_data)
--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 
 Write-Output "Starting Compilaton of Qt Ressource"
-pyside6-rcc "res.qrc" -o "src\lce_qt_launcher\res_rc.py" || exit /b 1
+pyside6-rcc "res.qrc" -o "src\lce_qt_launcher\res_rc.py"
 Write-Output "done"
 
 Write-Output "Compilation of Qt ui files"
@@ -11,7 +11,7 @@ $QT_UI_ARRAY="system_info","form","instance","settingDialog","about", "contentIn
 foreach ($QT_UI in $QT_UI_ARRAY)
 {
     Write-Output "Compiling $QT_UI.ui Qt UI file"
-    pyside6-uic "src/$T_UI..ui" -o "src\lce_qt_launcher\ui_$Qt_UI.py"
+    pyside6-uic "src\$QT_UI.ui" -o "src\lce_qt_launcher\ui_$Qt_UI.py"
     Write-Output "done"
 }
 
